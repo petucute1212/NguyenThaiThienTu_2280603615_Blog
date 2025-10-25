704491b9e7406dc86a8b16e7a2119481e3c06d96
petucute1212 <petucute1212@gmail.com>
2025-10-25 21:38:24 +0700

    First commit - my static blog

---
+++ js/data.js
@@ -0,0 +1,14 @@
+﻿const posts = [
+    {
+        title: "Giới thiệu về lập trình mạng với Java",
+        date: "2025-10-19",
+        tag: "Java",
+        link: "posts/post1.html"
+    },
+    {
+        title: "Kết nối API bằng JavaScript - Bước đầu với Fetch",
+        date: "2025-10-20",
+        tag: "JavaScript",
+        link: "posts/post2.html"
+    }
+];
---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("post-list");
+    posts.forEach(p => {
+        const div = document.createElement("div");
+        div.classList.add("post");
+        div.innerHTML = `
+      <h2><a href="${p.link}">${p.title}</a></h2>
+      <p><small>${p.date} | Chủ đề: ${p.tag}</small></p>
+    `;
+        container.appendChild(div);
+    });
+});
